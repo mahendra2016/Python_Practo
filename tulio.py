a7523adf3a6d5fa98911f56e8e83f449840d6601
mahendra2016 <prajapatimahendra916@gmail.com>
2016-08-28 22:25:23 +0530

    program python

---
+++ tulio.py
@@ -0,0 +1,10 @@
+##converter = float(raw_input("Enter a number: "))
+##convert = converter * 100.00
+##print "Centimeter :",convert
+
+from itertools import *
+name = raw_input['Enter a name: ']
+name = repeat(1,3)
+for nom in x:
+  print nom
+
